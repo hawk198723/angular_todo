358719fd656b1e604a5155ca47ceeaa857b02cc5
hawk198723 <hawk198723@gmail.com>
2023-11-12 16:20:18 -0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodoDetailComponent } from './todo-detail/todo-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    TodoDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todo';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css'],
+})
+export class TodoComponent implements OnInit {
+  todo: Todo = { id: 1, name: 'Take 5 min break' };
+
+  constructor() {}
+  ngOnInit(): void {}
+}
